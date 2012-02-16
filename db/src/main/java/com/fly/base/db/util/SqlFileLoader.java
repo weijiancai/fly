ec7c69760cf1513ec7678256e0c8f38c9d1a7e38
weijiancai <weijiancai@126.com>
2012-02-16 23:25:15 +0800

    SQL文件装载器

---
+++ db/src/main/java/com/fly/base/db/util/SqlFileLoader.java
@@ -0,0 +1,9 @@
+package com.fly.base.db.util;
+
+/**
+ * Sql文件装载器，可以从文件中读取sql语句，执行中的SQL语句
+ *
+ * @author weijiancai
+ */
+public class SqlFileLoader {
+}
---
+++ db/src/test/java/com/fly/base/db/util/SqlFileLoaderTest.java
@@ -0,0 +1,7 @@
+package com.fly.base.db.util;
+
+/**
+ * @author weijiancai
+ */
+public class SqlFileLoaderTest {
+}
